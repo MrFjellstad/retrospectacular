e396724644d6d132787fb511caa6fddb8ebfefc2
MrFjellstad <kent@fjellstad.name>
2014-02-15 00:28:16 +0100

    Removed duplicate code

--- service/routes.js
+++ service/routes.js
@@ -38,8 +38,6 @@ passport.use(new googleStrategy({
 }, function(identifier, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-
-
 // Passport session setup.
 // To support persistent login sessions, Passport needs to be able to
 // serialize users into and deserialize users out of the session. Typically,
@@ -52,7 +50,6 @@ passport.use(new googleStrategy({
     });
 }));
 
-
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -60,25 +57,6 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
-
-
-passport.use(new googleStrategy({
-    returnURL: 'http://localhost:3000/auth/google/return',
-    realm: 'http://localhost:3000/'
-}, function(identifier, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-        // To keep the example simple, the user's Google profile is returned to
-        // represent the logged-in user. In a typical application, you would want
-        // to associate the Google account with a user record in your database,
-        // and return that user instead.
-        profile.identifier = identifier;
-        return done(null, profile);
-        });
-    }
-));
-
-
 
 exports.setup = function (api, express) {
     api.use(express.cookieParser());
