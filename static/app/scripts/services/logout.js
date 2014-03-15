60deff8bb8c5696f93985e68a3668f13a59f258a
MrFjellstad <kent@fjellstad.name>
2014-03-15 16:13:32 +0100

    Logout service

---
+++ static/app/scripts/services/logout.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('retrospectApp')
+  .factory('Logout', ['$resource', 'SERVICE_URL',
+    function ($resource, SERVICE_URL) {
+      return $resource(SERVICE_URL + '/auth/loggout');
+    }
+  ]);
